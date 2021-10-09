289c7dcee4a7e96ab0032a06a7ef482768d495cd
Luis M <lmarin@messangi.com>
2021-10-09 14:44:59 -0500

    add step api

--- src/test/java/runners/mobile/BusquedaPropiedadRunner.java
+++ src/test/java/runners/mobile/BusquedaPropiedadRunner.java
@@ -6,7 +6,7 @@ import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
-@CucumberOptions(features = "src/test/resources/features/apis/v2/kingslanding/users.feature",
+@CucumberOptions(features = "src/test/resources/features/mobile/BuscandoPropiedad.feature",
         glue = "stepdefinitions",
         tags = "@TC001",
         snippets = SnippetType.CAMELCASE)
