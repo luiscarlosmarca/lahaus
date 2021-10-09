32e8623972a498087ddf9d31309296584a5cc9c6
Luis M <lmarin@messangi.com>
2021-10-09 15:47:26 -0500

    add task buscar propiedades

---
+++ src/main/java/tasks/mobile/SeleccionaUbicacionPropiedad.java
@@ -0,0 +1,34 @@
+package tasks.mobile;
+
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Task;
+import net.serenitybdd.screenplay.actions.Click;
+
+import static net.serenitybdd.screenplay.Tasks.instrumented;
+import static ui.mobile.Android.ParametrosDeBusquedaScreen.*;
+
+public class SeleccionaUbicacionPropiedad implements Task {
+
+    String pais,ciudad;
+
+    public SeleccionaUbicacionPropiedad(String pais) {
+        this.pais = pais;
+
+    }
+
+    public static SeleccionaUbicacionPropiedad seleccionaEl(String pais) {
+        return instrumented(SeleccionaUbicacionPropiedad.class,pais);
+    }
+
+    public SeleccionaUbicacionPropiedad yLa(String ciudad){
+        this.ciudad=ciudad;
+        return this;
+    }
+    @Override
+
+    public <T extends Actor> void performAs(T actor) {
+        actor.attemptsTo(Click.on(BTN_SELECCIONAR_PAIS.of(pais)));
+        actor.attemptsTo(Click.on(BTN_SELECCIONAR_CIUDAD.of(ciudad)));
+
+    }
+}
